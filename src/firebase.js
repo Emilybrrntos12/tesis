bf3a935e2ab1872e74351fcc6bafb45a2db3f91e
Emily Barrientos <emi.brrntos12@gmail.com>
2025-08-24 22:40:08 -0600

    .evn

--- src/firebase.js
+++ src/firebase.js
@@ -6,14 +6,14 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD6q6uH0jrcC_nQXe3-ETyzpJ7fMqodKIQ",
-  authDomain: "eb12-e14d5.firebaseapp.com",
-  databaseURL: "https://eb12-e14d5-default-rtdb.firebaseio.com",
-  projectId: "eb12-e14d5",
-  storageBucket: "eb12-e14d5.firebasestorage.app",
-  messagingSenderId: "618179108980",
-  appId: "1:618179108980:web:288773506479094457f121",
-  measurementId: "G-DJHTLS6MJT"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,     // ✅ Aquí
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,    
 };
 
 // Initialize Firebase
